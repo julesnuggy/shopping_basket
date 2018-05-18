2e313e9e8a762226abe1947520a233c9087a9412
julesnuggy <julesnuggy@gmail.com>
2018-05-18 18:27:12 +0100

    GREEN: BasketItem#create test passing

--- src/basketItem.js
+++ src/basketItem.js
@@ -1,1 +1,13 @@
 'use strict'
+
+class BasketItem {
+  constructor() {
+    this._title = ''
+  }
+
+  create(item) {
+    this._title = item._title;
+  }
+}
+
+module.exports = BasketItem;
--- test/basketItemTest.js
+++ test/basketItemTest.js
@@ -1,20 +1,22 @@
 describe('Basket Item', function() {
   const expect = require('chai').expect;
+  const sinon = require('sinon');
   const BasketItem = require('../src/basketItem.js');
   const SetQuantity = require('../src/setQuantity.js')
 
   let firstItem, mountainDew, setQuantity, calcSubtotal
 
-  describe('#create', function() {
-    beforeEach(function(){
-      firstItem = new BasketItem();
-      mountainDew = {_title: 'Mountain Dew', _unitCost: 3.6};
-    })
+  beforeEach(function(){
+    firstItem = new BasketItem();
+    mountainDew = {_title: 'Mountain Dew', _unitCost: 3.6};
+    // quantityStub = sinon.stub(SetQuantity, 'update');
+    // quantityReturns = quantityStub.returns(2);
+  })
 
+  describe('#create', function() {
     it('generates a new basket item', function() {
-      quantityStub = sinon.stub(SetQuantity, 'update')
-      quantityReturns = quantityStub.returns(2);
-      firstItem.create(mountainDew, setQuantity, calcSubtotal)
+      firstItem.create(mountainDew)
+      expect(firstItem._title).to.equal('Mountain Dew')
     })
   })
 })
