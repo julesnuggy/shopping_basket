8d3294082e4b6d9323ca2b55d22f645a42254df2
julesnuggy <julesnuggy@gmail.com>
2018-05-19 16:40:00 +0100

    Fixed broken tests in basketItemTest

--- test/basketItemTest.js
+++ test/basketItemTest.js
@@ -1,6 +1,7 @@
 describe('Basket Item', function() {
   const expect = require('chai').expect;
   const sinon = require('sinon');
+  const localStorage = require('mock-local-storage')
   const BasketItem = require('../src/basketItem.js');
   const Calculate = require('../src/calculate.js')
 
@@ -20,15 +21,18 @@ describe('Basket Item', function() {
 
   describe('#updateQuantity', function() {
     it('updates the quantity of the basket item', function() {
-      expect(firstItem._quantity).to.equal(1);
-      firstItem.updateQuantity(3);
+      global.window = {}
+      global.div = {}
+      window.localStorage = global.localStorage
+      expect(firstItem._quantity).to.equal(0);
+      firstItem.updateQuantity(3, 'sessVar', global.div);
       expect(firstItem._quantity).to.equal(3);
     })
   })
 
   describe('#updateSubTotal', function() {
     it('updates the total for the basket item', function() {
-      expect(firstItem._subTotal).to.equal(3.6);
+      expect(firstItem._subTotal).to.equal(0);
       calcStub.subTotal.returns(10.8);
       firstItem.updateSubTotal();
       expect(firstItem._subTotal).to.equal(10.8);
